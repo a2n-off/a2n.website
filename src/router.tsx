046a3af8bc9a354bdce1efc031f2c30cb591ac3d
Alan Bouteiller <bouteiller.alan@nebulae.co>
2021-06-13 11:51:39 +0200

    🎉 feat(init): react

---
+++ src/router.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style/app.css';
+
+function Router() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/Router.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Router;
